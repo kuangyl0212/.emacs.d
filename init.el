aa67aeaa67ab060e56f591965af44bcf7b8869ff
forest <kuangyl0212@gmail.com>
2021-10-06 15:02:35 +0800

    update

--- init.el
+++ init.el
@@ -1,3 +1,11 @@
+;;; init.el --- Summary:
+
+;; Author: Foret
+
+;;; Commentary:
+
+;;; Code:
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -5,9 +13,8 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
- '(custom-enabled-themes (quote (misterioso)))
- '(package-selected-packages (quote (restart-emacs evil sml-mode))))
- (setq-default tab-width 4)
+ '(custom-enabled-themes '(misterioso))
+ '(package-selected-packages '(ace-window company restart-emacs evil sml-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -15,12 +22,15 @@
  ;; If there is more than one, they won't work right.
  )
 
+(setq-default tab-width 4)
+(scroll-bar-mode -1)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (setq inhibit-startup-screen t)
 
-(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(setq package-archives
+	  '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+        ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 (package-initialize) ;; You might already have this line
 (setq package-check-signature nil)
 
@@ -39,9 +49,41 @@
         use-package-always-defer t
         use-package-expand-minimally t)
   (require 'use-package))
+(use-package flycheck
+  :hook (after-init . global-flycheck-mode))
 
 (require 'package)
 (use-package evil)
 (use-package restart-emacs)
+(use-package gruvbox-theme
+  :init (load-theme 'gruvbox-dark-soft t))
+(use-package smart-mode-line
+  :init
+  (setq sml/no-confirm-load-theme t)
+  (setq sml/theme 'respectful)
+  (sml/setup))
+;; Settings for company
+(use-package company
+  :diminish
+  :defines (company-dabbrev-ignore-case company-dabbrev-downcase)
+  :init (add-hook 'after-init-hook 'global-company-mode)
+  :config (setq company-minimum-prefix-length 1
+                company-selection-wrap-around t))
+(use-package sml-mode)
+(use-package ace-window
+  :ensure t
+  :init (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)
+              aw-char-position 'left
+              aw-ignore-current nil
+              aw-leading-char-style 'char
+              aw-scope 'frame)
+  :bind (("M-o" . ace-window)
+         ("M-O" . ace-swap-window)))
 
 (evil-mode 1)
+(setq evil-want-C-d-scroll nil)
+(setq display-line-number-type t)
+(global-display-line-numbers-mode t)
+(provide 'init)
+
+;;; init.el ends here
